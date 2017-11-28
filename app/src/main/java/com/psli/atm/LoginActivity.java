5ee61b0507d6112536e5c04d98f7c45e57a4f860
psli0101 <p.s.li19980101@gmail.com>
2017-11-28 11:02:17 +0800

    add login

--- app/src/main/java/com/psli/atm/LoginActivity.java
+++ app/src/main/java/com/psli/atm/LoginActivity.java
@@ -2,12 +2,24 @@ package com.psli.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText edUserId = (EditText) findViewById( R.id.userid );
+    private EditText edPassWD = (EditText) findViewById( R.id.passwd );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
+    }
+
+    public void cancel(View view) {
+
     }
 }
