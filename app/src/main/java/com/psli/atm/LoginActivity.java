1640c1c115da162dcfa19aeb2a9b0bff8736b3ee
psli0101 <p.s.li19980101@gmail.com>
2017-11-28 11:54:42 +0800

    update

--- app/src/main/java/com/psli/atm/LoginActivity.java
+++ app/src/main/java/com/psli/atm/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.psli.atm;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText edUserId = (EditText) findViewById( R.id.userid );
-    private EditText edPassWD = (EditText) findViewById( R.id.passwd );
+    private EditText edUserId, edPassWD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +18,33 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-
+        this.edUserId = (EditText) findViewById( R.id.userid );
+        this.edPassWD = (EditText) findViewById( R.id.passwd );
+        String id = edUserId.getText().toString();
+        String pw = edPassWD.getText().toString();
+        if ( "PS".equals(id) && "1234".equals(pw) ) {
+            Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setMessage("You enter wrong ID or Password.")
+                    .setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    set();
+                                }
+                            })
+                    .show();
+        }
     }
 
     public void cancel(View view) {
+        
+    }
 
+    private void set() {
+        this.edUserId.setText("");
+        this.edPassWD.setText("");
     }
 }
