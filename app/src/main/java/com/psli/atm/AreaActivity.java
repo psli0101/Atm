41c726748d695a419a76f48ef2fa48f5e6c96b2c
psli0101 <p.s.li19980101@gmail.com>
2017-12-26 12:03:55 +0800

    update City $ Area Activities

--- app/src/main/java/com/psli/atm/AreaActivity.java
+++ app/src/main/java/com/psli/atm/AreaActivity.java
@@ -3,10 +3,14 @@ package com.psli.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class AreaActivity extends AppCompatActivity {
+public class AreaActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private String[] area;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,21 +19,29 @@ public class AreaActivity extends AppCompatActivity {
         ListView ls_area = (ListView) findViewById(R.id.areas);
         Intent intent = getIntent();
         int city = intent.getIntExtra("CITY_EXTRA", 0);
-        String[] area = null;
+        area = null;
         switch (city) {
             case 0:
                 String[] data = {"板橋區", "永和區", "新莊區"};
-                area = data;
+                this.area = data;
                 break;
             case 1:
                 String[] data2 = {"信義區", "大安區", "士林區"};
-                area = data2;
+                this.area = data2;
                 break;
             case 2:
                 String[] data3 = {"中正區", "暖暖區", "八堵區"};
-                area = data3;
+                this.area = data3;
                 break;
         }
         ls_area.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, area));
+        ls_area.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        getIntent().putExtra("AREA_EXTRA", area[i]);
+        setResult(RESULT_OK, getIntent());
+        finish();
     }
 }
--- app/src/main/java/com/psli/atm/CityActivity.java
+++ app/src/main/java/com/psli/atm/CityActivity.java
@@ -1,12 +1,13 @@
 package com.psli.atm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class CityActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
@@ -26,6 +27,15 @@ public class CityActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Intent intent = new Intent(this, AreaActivity.class);
         intent.putExtra("CITY_EXTRA", i);
-        startActivity(intent);
+        startActivityForResult(intent, 200);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            String area = data.getStringExtra("AREA_EXTRA");
+            Toast.makeText(this, area, Toast.LENGTH_SHORT).show();
+        }
     }
 }
